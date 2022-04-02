8a130c159bc7ab4bd095f4c8fc7927f2283476ac
yusanxing <32813254+yusanxing@users.noreply.github.com>
2022-04-02 21:21:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,24 +15,22 @@
 
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #========================================================================================================
 
 # git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 
 #========================================================================================================
-git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
+# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
-# git clone https://github.com/small-5/luci-app-adblock-plus.git package/luci-app-adblock-plus
 # git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
 #========================================================================================================
 # git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
 # git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
 # git clone https://github.com/NateLol/luci-app-beardropper.git package/beardropper
 # git clone https://github.com/apple/cups.git package/cups
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
 # git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-
 # https://github.com/jerrykuku/luci-theme-argon
