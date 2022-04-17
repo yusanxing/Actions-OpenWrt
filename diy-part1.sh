1692c57aaa032c5c020960d883c29a5434c11486
yusanxing <32813254+yusanxing@users.noreply.github.com>
2022-04-17 21:28:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,29 +14,21 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-   
-# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
-     
-  echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
- 
-#  echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#  echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git liuran001_packages https://github.com/liuran001/openwrt-packages' >>feeds.conf.default
 
 
 #========================================================================================================
-# svn co https://github.com/liuran001/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-clash
-# svn co https://github.com/liuran001/openwrt-packages/trunk/luci-app-jd-dailybonus package/luci-app-jd-dailybonus
+git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
 
-# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
-# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
-# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
-# git clone https://github.com/project-lede/luci-app-godproxy.git package/godproxy
+# git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/jerrykuku-jd-dailybonus
+# git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
+# git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
+
+# git clone https://github.com/project-lede/luci-app-godproxy.git package/lean/godproxy
 #========================================================================================================
-# git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/luci-app-autotimeset
-# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/iptvhelper
-# git clone https://github.com/NateLol/luci-app-beardropper.git package/beardropper
-# git clone https://github.com/apple/cups.git package/cups
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/sirpdboy/luci-app-autotimeset.git package/lean/luci-app-autotimeset
+# git clone https://github.com/riverscn/openwrt-iptvhelper.git package/lean/iptvhelper
+# git clone https://github.com/NateLol/luci-app-beardropper.git package/lean/beardropper
+# git clone https://github.com/destan19/OpenAppFilter.git package/lean/OpenAppFilter
 # https://github.com/jerrykuku/luci-theme-argon
