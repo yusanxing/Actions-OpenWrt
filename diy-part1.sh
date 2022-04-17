2b9468c352940c37e1fe7890b182332abbd32c8b
yusanxing <32813254+yusanxing@users.noreply.github.com>
2022-04-17 09:21:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,11 +21,9 @@
  echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
  echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-#========================================================================================================
-
-# git clone https://github.com/kenzok8/openwrt-packages.git package/kenzok8
 
 #========================================================================================================
+svn co https://github.com/liuran001/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 # git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/jerrykuku-jd-dailybonus
 # git clone https://github.com/kongfl888/luci-app-adguardhome.git package/adguardhome
 # git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
