d2b5db3dce531b1135014062409b9d6e18189c95
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-21 14:09:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,5 +19,7 @@ sed -i 's/192.168.1.1/10.0.0.33/g' package/base-files/files/bin/config_generate
 # 删除固件默认登录密码
 #sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 
-#取消bootstrap为默认主题f
-#sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#取消bootstrap为默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+# 替换默认主题为luci-theme-argon(可选)
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
