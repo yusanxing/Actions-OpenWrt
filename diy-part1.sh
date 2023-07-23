3e89d13a76b01697ac26e884cac4fb88671fce23
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-23 20:56:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,16 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+#echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 #sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 #sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #========================================================================================================
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+         
+
 #git clone https://github.com/kongfl888/luci-app-adguardhome.git package/lean/adguardhome
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/lean/jerrykuku-jd-dailybonus
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/lean/luci-app-smartdns
