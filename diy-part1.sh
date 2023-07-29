ab38e0eae797b8298147788ca9f1b1b20a1a78a7
yusanxing <32813254+yusanxing@users.noreply.github.com>
2023-07-29 13:06:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,9 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
+# echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages' >>feeds.conf.default
 
-#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 #========================================================================================================
 #git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
